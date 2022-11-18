6b2c817cab4880cff40c28b78ba708f0c76c2be2
giulianacarabettaow <gcarabettaowens@udesa.edu.ar>
2022-11-18 15:19:32 -0300

    conecte api de pelis mas valoradas

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-let categories=document.querySelector(".categories.masvistas");
-    categories.style.display="flex";
-    categories.style.flexWrap="wrap";
+let categoriesMasVistas=document.querySelector(".categories.masvistas");
+    categoriesMasVistas.style.display="flex";
+    categoriesMasVistas.style.flexWrap="wrap";
     
-let info = ''
+let infoMasVistas = ''
 
 //let categories=""
 
@@ -13,13 +13,36 @@ fetch("https://api.themoviedb.org/3/movie/popular?api_key=ba0b591fbb4dcbf21e7a27
 .then(function(data){
     console.log(data);
     for(let i=0; i < 8; i++){
-        info +=`
+        infoMasVistas +=`
         <li>
         <img src= 'https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}' alt=''/>
         <p>Name: ${data.results[i].original_title}</p>
         </li>`
     }
-    categories.innerHTML= info
+    categoriesMasVistas.innerHTML= infoMasVistas
+})
+.catch(function(error){
+    console.log("Error: " + error);
+})
+
+let categoriesMasValoradas=document.querySelector(".categories.masvaloradas");
+    categoriesMasValoradas.style.display="flex";
+    categoriesMasValoradas.style.flexWrap="wrap";
+let infoMasValoradas = ''
+
+fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=c71f5b75c8e3c6372967558c16ff597f")
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data);
+    for(let i=0; i<8; i++){
+        infoMasValoradas += `
+        <li> <img src= 'https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}' alt='' />
+        <p> Name: ${data.results[i].original_title}</p>
+        </li>`
+    }
+    categoriesMasValoradas.innerHTML= infoMasValoradas
 })
 .catch(function(error){
     console.log("Error: " + error);
