c04c5f3418b2978a477432fdc08e258b3099eaab
IgnacioTettamanti <itettamanti@udesa.edu.ar>
2022-11-18 15:17:23 -0300

    js series

--- js/index.js
+++ js/index.js
@@ -4,8 +4,6 @@ let categories=document.querySelector(".categories.masvistas");
     
 let info = ''
 
-//let categories=""
-
 fetch("https://api.themoviedb.org/3/movie/popular?api_key=ba0b591fbb4dcbf21e7a279fceca5d5e")
 .then(function(response){
     return response.json()
@@ -24,3 +22,74 @@ fetch("https://api.themoviedb.org/3/movie/popular?api_key=ba0b591fbb4dcbf21e7a27
 .catch(function(error){
     console.log("Error: " + error);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let categoriesSeriesMasValoradas=document.querySelector(".categories.seriesmasvaloradas")
+categoriesSeriesMasValoradas.style.display="flex";
+categoriesSeriesMasValoradas.style.flexWrap="wrap"
+
+    let infoSMasValoradas = ''
+
+fetch("https://api.themoviedb.org/3/tv/top_rated?api_key=c71f5b75c8e3c6372967558c16ff597f")}
+    .then(function(response){
+        return response.json()
+    })
+
+    .then(function(data){
+        console.log(data);
+        for(let i=0; i < 8; i++){
+            info +=`
+            <li>
+            <img src= 'https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}' alt=''/>
+            </li>`
+        }
+    })
+
+    .catch(function(error){
+        console.log("Error: " + error);
+    })
+    
