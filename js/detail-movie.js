0689bb2d165e2d91cd63d1b0ddd989d23fa223f0
IgnacioTettamanti <itettamanti@udesa.edu.ar>
2022-11-19 15:54:52 -0300

    .

--- js/detail-movie.js
+++ js/detail-movie.js
@@ -28,7 +28,7 @@ fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=c71f5b75c8e3c6372967558c
         <ul class="ulDetail">
         <li>Fecha de lanzamiento:${data.release_date}</li>
         <li>Calificación: ${data.vote_average} </li>
-    
+        
         <li>Duración: ${data.runtime} minutos </li>
         </ul>
         `
@@ -44,7 +44,6 @@ fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=c71f5b75c8e3c6372967558c
     
     let genres=document.querySelector(".genres")
 
-    let infoGenres= ""
 
     fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=c71f5b75c8e3c6372967558c16ff597f`)
     .then(function(response){
@@ -52,12 +51,15 @@ fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=c71f5b75c8e3c6372967558c
     })
     .then(function(data){
         console.log(data)
-        for (let i=0; i> data.results.length; i++)
-        genres +=`
-      
+
+        let infoGenres=''
+
+        for (let i=0;i>data.results.length; i++){
+        
+        infoGenres += `
         <li>Géneros: ${data.results[i].genres}</li>
-      
         `
+    }
         genres.innerHTML=infoGenres
     })
     
@@ -65,29 +67,6 @@ fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=c71f5b75c8e3c6372967558c
         console.log("Error: " + error);
     })
 
+    
 
-    let detailMovieProvider = document.querySelector(".provider")
-    let infoProvider=''
-
-
-    fetch(`https://api.themoviedb.org/3/movie/${id}/watch/providers?api_key=c71f5b75c8e3c6372967558c16ff597f`)   
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
-        console.log(data)
-        
-        for (let i=0; i<data.results.us; i++ ){
-            infoProvider +=`
-            <li>
-                <img src='https://image.tmdb.org/t/p/w500/ ${data.results[i].logo_path}'>
-            </li>
-            `
-        }
-
-    })
-
-    .catch(function(error){
-        console.log("Error: " + error);
-    })
 
