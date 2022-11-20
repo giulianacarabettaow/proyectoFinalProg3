05d20224f47042bff7479eced0de42d6eca2fa0b
IgnacioTettamanti <itettamanti@udesa.edu.ar>
2022-11-20 00:37:50 -0300

    terminando genres html y empezando genresdetail.html

--- js/genres.js
+++ js/genres.js
@@ -6,17 +6,35 @@ fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=c71f5b75c8e3c637296
     })
     .then(function(data){
         console.log(data)
-        console.log(data.genres)
-        console.log(data.genres.length)
-        let genreListM=''
-        for(let i=0;i<data.genres.length;i++){
-            genreListM +=`
-            <li class="liGenreListM>${data.genres[i].name}</li>
+        let genrelistM=''
+        for(i=0;i<data.genres.length;i++){
+            genrelistM+=`//*<a href="./detail-genres.html?id=${data.genres[i].id}*//
+            <li>${data.genres[i].name}
             `
-        moviesGenres.innerHTML=genreListM
+            moviesGenres.innerHTML=genrelistM
         }
     })
+
     .catch(function(error){
         console.log("Error: " + error);
     })
 
+    let seriesGenres=document.querySelector(".genrelistS")
+
+    fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=c71f5b75c8e3c6372967558c16ff597f")
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data){
+            console.log(data)
+            let genrelistS=''
+            for(i=0;i<data.genres.length;i++){
+                genrelistS+=`<li>${data.genres[i].name}</li>
+                `
+                seriesGenres.innerHTML=genrelistS
+            }
+        })
+
+        .catch(function(error){
+            console.log("Error: " + error);
+        })
